5393a99726901c61c1fe5117d77d145ac81088b2
Sid <SidBayeck@outlook.com>
2023-03-26 11:20:45 +0800

    remove nixvim

--- flake.nix
+++ flake.nix
@@ -10,8 +10,8 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixvim.url = "github:pta2002/nixvim";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    # nixvim.url = "github:pta2002/nixvim";
+    # nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { nixpkgs, home-manager, sinur, ... } @ inputs:
