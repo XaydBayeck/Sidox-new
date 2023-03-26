15890813239540c8cfa980ae4de07df58ede75f6
Sid <SidBayeck@outlook.com>
2023-03-26 16:50:30 +0800

    add language servers

--- home/sid/develop.nix
+++ home/sid/develop.nix
@@ -4,5 +4,11 @@
   home.packages = with pkgs; [
     ghc
     # gulie_3_0
+    # NOTE: Language Servers
+    rust-analyzer
+    haskell-language-server
+    sumneko-lua-language-server
+    nil
+    clang-tools
   ];
 }
