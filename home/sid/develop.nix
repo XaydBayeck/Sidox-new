738b15cb63ae7395993f41fe26a92f60950c2cdd
Sid <SidBayeck@outlook.com>
2023-03-30 19:56:25 +0800

    add emacs

--- home/sid/develop.nix
+++ home/sid/develop.nix
@@ -3,7 +3,8 @@
 {
   home.packages = with pkgs; [
     ghc
-    # gulie_3_0
+    gulie_3_0
+    chez
     # NOTE: Language Servers
     rust-analyzer
     haskell-language-server
@@ -11,4 +12,73 @@
     nil
     clang-tools
   ];
+
+  programs.emacs = {
+    enable = true;
+    extraPackages = epkgs: with epkgs;
+    let
+      config = [
+        use-package
+      ];
+      appearance = [
+        doom-themes
+        doom-modline
+        dashboard
+        rainbow-mode
+        rainbow-delimiters
+        highlight-indent-guides
+      ];
+      operate = [
+        meow
+        which-key
+      ];
+      company = [
+        vertico
+        orderless
+        marginalia
+        embark
+        consult
+        embark-consult
+      ];
+      corfu = [
+        corfu
+        corfu-doc
+        kind-icon
+        cape
+      ];
+      edit = [
+        tree-sitter
+        tree-sitter-langs
+        flycheck
+        eglot
+        flycheck-eglot
+        geiser
+        geiser-guile
+        geiser-chez
+        macrostep-geiser
+        sly
+        parinfer-rust-mode
+        smartparens
+        yasnippet
+        yasnippet-snippets
+      ];
+      manager = [
+        treemacs
+        projectile
+        magit
+      ];
+      dependencies = [
+        posframe
+      ];
+    in builtins.concatLists [
+      appearance
+      operate
+      config
+      company
+      corfu
+      edit
+      manager
+      dependencies
+    ];
+  };
 }
