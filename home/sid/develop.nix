dfdd637cedf051d1679a18de46174fdba980717a
Sid <SidBayeck@outlook.com>
2023-03-30 20:08:05 +0800

    add emacs

--- home/sid/develop.nix
+++ home/sid/develop.nix
@@ -17,69 +17,69 @@
   programs.emacs = {
     enable = true;
     extraPackages = epkgs: with epkgs;
-    let
-      config = [
-        use-package
-      ];
-      appearance = [
-        doom-themes
-        doom-modeline
-        dashboard
-        rainbow-mode
-        rainbow-delimiters
-        highlight-indent-guides
-      ];
-      operate = [
-        meow
-        which-key
-      ];
-      company = [
-        vertico
-        orderless
-        marginalia
-        embark
-        consult
-        embark-consult
-      ];
-      corfu = [
-        corfu
-        corfu-doc
-        kind-icon
-        cape
-      ];
-      edit = [
-        tree-sitter
-        tree-sitter-langs
-        flycheck
-        eglot
-        flycheck-eglot
-        geiser
-        geiser-guile
-        geiser-chez
-        macrostep-geiser
-        sly
-        parinfer-rust-mode
-        smartparens
-        yasnippet
-        yasnippet-snippets
-      ];
-      manager = [
-        treemacs
-        projectile
-        magit
-      ];
-      dependencies = [
-        posframe
-      ];
-    in builtins.concatLists [
+      let
+        config = [
+          use-package
+        ];
+        appearance = [
+          doom-themes
+          doom-modeline
+          dashboard
+          rainbow-mode
+          rainbow-delimiters
+          highlight-indent-guides
+        ];
+        operate = [
+          meow
+          which-key
+        ];
+        company = [
+          vertico
+          orderless
+          marginalia
+          embark
+          consult
+          embark-consult
+        ];
+        corfu = [
+          corfu
+          corfu-doc
+          kind-icon
+          cape
+        ];
+        edit = [
+          tree-sitter
+          tree-sitter-langs
+          flycheck
+          eglot
+          flycheck-eglot
+          geiser
+          geiser-guile
+          geiser-chez
+          macrostep-geiser
+          sly
+          parinfer-rust-mode
+          smartparens
+          yasnippet
+          yasnippet-snippets
+        ];
+        manager = [
+          treemacs
+          projectile
+          magit
+        ];
+        dependencies = [
+          posframe
+        ];
+      in
       appearance
-      operate
-      config
-      company
-      corfu
-      edit
-      manager
-      dependencies
-    ];
+      ++ operate
+      ++ config
+      ++ company
+      ++ corfu
+      ++ edit
+      ++ manager
+      ++ dependencies
+    ;
   };
 }
