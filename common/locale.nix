a49420222006575007c27659e1ed2883531ead37
Sid <SidBayeck@outlook.com>
2023-04-19 17:54:46 +0800

    add env vars

--- common/locale.nix
+++ common/locale.nix
@@ -31,16 +31,10 @@
     LC_TIME = "zh_CN.UTF-8";
   };
 
-  environment.sessionVariables = rec {
+  environment.sessionVariables = {
     XDG_CACHE_HOME  = "$HOME/.cache";
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_DATA_HOME   = "$HOME/.local/share";
     XDG_STATE_HOME  = "$HOME/.local/state";
-
-    # Not officially in the specification
-    XDG_BIN_HOME    = "$HOME/.local/bin";
-    PATH = [ 
-      "${XDG_BIN_HOME}"
-    ];
   };
 }
