1baed6164f530bc07f8a1cb5962ca3db014644b9
Sid <SidBayeck@outlook.com>
2023-04-20 19:54:54 +0800

    add nushell scripts

--- home/sid/shell.nix
+++ home/sid/shell.nix
@@ -24,37 +24,37 @@
           size = 12;
         };
         colors = {
-        primary = {
-          background = "0x2b3339";
-          foreground = "0xd8caac";
-        };
+          primary = {
+            background = "0x2b3339";
+            foreground = "0xd8caac";
+          };
 
-        normal = {
-          black = "0x090618";
-          red = "0xc34043";
-          green = "0x76946a";
-          yellow = "0xc0a36e";
-          blue = "0x7e9cd8";
-          magenta = "0x957fb8";
-          cyan = "0x6a9589";
-          white = "0xc8c093";
-        };
-        bright = {
-          black = "0x727169";
-          red = "0xe82424";
-          green = "0x98bb6c";
-          yellow = "0xe6c384";
-          blue = "0x7fb4ca";
-          magenta = "0x938aa9";
-          cyan = "0x7aa89f";
-          white = "0xdcd7ba";
-        };
+          normal = {
+            black = "0x090618";
+            red = "0xc34043";
+            green = "0x76946a";
+            yellow = "0xc0a36e";
+            blue = "0x7e9cd8";
+            magenta = "0x957fb8";
+            cyan = "0x6a9589";
+            white = "0xc8c093";
+          };
+          bright = {
+            black = "0x727169";
+            red = "0xe82424";
+            green = "0x98bb6c";
+            yellow = "0xe6c384";
+            blue = "0x7fb4ca";
+            magenta = "0x938aa9";
+            cyan = "0x7aa89f";
+            white = "0xdcd7ba";
+          };
 
-        selection = {
-          background = "0x2d4f67";
-          foreground = "0xc8c093";
+          selection = {
+            background = "0x2d4f67";
+            foreground = "0xc8c093";
+          };
         };
-      };
       };
     };
     fish = {
@@ -103,30 +103,26 @@
       enable = true;
       # package = pkgs.nushell.override { additionalFeatures = (p: p ++ ["dataframe"]); };
       configFile.source = ./config.nu;
-      extraConfig = ''
-        let-env NU_LIB_DIRS = [
-          '${config.home.homeDirectory}/.config/nushell/scripts'
-        ]
+      extraConfig =
+        let
+          scripts = "${pkgs.nu_scripts}";
+        in
+        ''
+          let-env NU_LIB_DIRS = [
+            '${config.home.homeDirectory}/.config/nushell/scripts'
+          ]
 
-        if ('${config.home.homeDirectory}/.config/nushell/custom.nu' | path exists) {
-          use ${config.home.homeDirectory}/.config/nushell/custom.nu *
-        }
-      '';
-      # let
-      #   scripts = "${pkgs.nu_scripts}";
-      # in
-      #     # \'${home.homeDirectory}/.config/nushell/scripts\'
-      #  ''
-      #   let-env NU_LIB_DIRS = [
-      #     '${scripts}'
-      #   ]
-      #   # Custom Completions
-      #   use ${scripts}/custom_completions/git/git-completions.nu *
-      #   use ${scripts}/custom_completions/nix/nix-completions.nu *
-      #   use ${scripts}/custom_completions/cargo/cargo-completions.nu *
-      #   use ${scripts}/custom_completions/btm/btm-completions.nu *
-      #   use ${scripts}/custom_completions/make/make-completions.nu *
-      # '';
+          if ('${config.home.homeDirectory}/.config/nushell/custom.nu' | path exists) {
+            use ${config.home.homeDirectory}/.config/nushell/custom.nu *
+          }
+
+          # Custom Completions
+          use ${scripts}/custom_completions/git/git-completions.nu *
+          use ${scripts}/custom_completions/nix/nix-completions.nu *
+          use ${scripts}/custom_completions/cargo/cargo-completions.nu *
+          use ${scripts}/custom_completions/btm/btm-completions.nu *
+          use ${scripts}/custom_completions/make/make-completions.nu *
+        '';
       extraEnv = ''
         let-env FORGIT = ${pkgs.fishPlugins.forgit}/share/fish/vendor_conf.d/bin/git-forgit
       '';
