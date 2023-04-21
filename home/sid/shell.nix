91787a02499fe012f4da9ce00d4775b5d731cc78
Sid <SidBayeck@outlook.com>
2023-04-21 10:11:21 +0800

    add wezter config

--- home/sid/shell.nix
+++ home/sid/shell.nix
@@ -5,6 +5,7 @@
   programs = {
     wezterm = {
       enable = true;
+      extraConfig = builtins.readFile ./wezterm.lua;
     };
     alacritty = {
       enable = true;
---
+++ home/sid/wezterm.lua
@@ -0,0 +1,47 @@
+-- Add config folder to watchlist for config reloads.
+local wezterm = require 'wezterm';
+wezterm.add_to_config_reload_watch_list(wezterm.config_dir)
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+config.fornt = wezterm.font 'Hurmit Nerd Font'
+config.color_scheme = 'tokyonight_storm'
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.85
+
+config.launch_menu = {
+  {
+    args = { 'top' },
+  },
+  {
+    label = 'Bash',
+    args = { 'bash', '-l' },
+  },
+  {
+    label = 'fish',
+    args = { 'fish' },
+  },
+  {
+    label = 'nushell',
+    args = { 'nu' },
+  },
+  {
+    label = 'nix repl',
+    args = { 'nix', 'repl', '-I', 'nixpkgs=flake:nixpkgs' }
+  },
+  {
+    label = 'julia',
+    args = {'julia'},
+  }
+}
+
+-- and finally, return the configuration to wezterm
+return config
