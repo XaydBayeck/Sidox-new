1b7b3a37a36858e75ce87e41d8fec1c55841e40a
Sid <SidBayeck@outlook.com>
2023-04-27 14:05:41 +0800

    enable xdg in hm

--- overlays/teaks.nix
+++ overlays/teaks.nix
@@ -1,16 +1,5 @@
 self: super:
 
 {
-  # leftwm = super.leftwm.overrideAttrs (oldAttrs:
-  #   let
-  #     rpathLibs = oldAttrs.buildInputs;
-  #   in
-  #   {
-  #     postInstall = ''
-  #       for p in $out/bin/left*; do
-  #         patchelf --set-rpath "${super.lib.makeLibraryPath rpathLibs}" $p
-  #       done
-  #     '';
-  #   });
-  nushell = super.nushell.override { additionalFeatures = (p: p ++ ["dataframe"]); };
+  nushell = super.nushell.override { additionalFeatures = (p: p ++ [ "dataframe" ]); };
 }
