0b3b2901fd2b40179e38e523a2d94e6057848b44
Sid <SidBayeck@outlook.com>
2023-09-17 22:39:18 +0800

    No need mirror

--- common/nix-config.nix
+++ common/nix-config.nix
@@ -4,9 +4,9 @@
   nixpkgs.config.allowUnfree = true;
 
   nix.settings = {
-    substituters = [
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-    ];
+    #substituters = [
+    #  "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+    #];
     experimental-features = [ "nix-command" "flakes" ];
     trusted-users = [ "root" "sid" ];
     auto-optimise-store = true;
