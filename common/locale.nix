ae6d27e01d85022e8270660a8a7b5c3bfdecf207
Sid <SidBayeck@outlook.com>
2023-11-30 14:25:00 +0800

    flake update

--- common/locale.nix
+++ common/locale.nix
@@ -20,6 +20,8 @@
   i18n.supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
 
   i18n.extraLocaleSettings = {
+    LC_ALL = "en_US.UTF-8";
+
     LC_ADDRESS = "zh_CN.UTF-8";
     LC_IDENTIFICATION = "zh_CN.UTF-8";
     LC_MEASUREMENT = "zh_CN.UTF-8";
