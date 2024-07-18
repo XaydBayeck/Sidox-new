10e47799f66c712b8f18700247e97020bc341fde
Sid <SidBayeck@outlook.com>
2024-07-18 20:56:54 +0800

    deprached i18n.inputMethod.enabled="fcitx5"

--- common/locale.nix
+++ common/locale.nix
@@ -3,7 +3,8 @@
 {
   # Select input method
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-gtk
       fcitx5-rime
